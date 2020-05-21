260736538b4d3873d3d59bad4824b44190a09251
Sungsoo Kim <sskim950105@gmail.com>
2020-05-21 13:49:08 +0900

    Following tutorials Create RESTful API

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
+// Getting all
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// Getting one
+router.get('/:id', (req, res) => {
+
+})
+
+// Creating one
+router.post('/', (req, res) => {
+
+})
+
+// Updating one
+router.patch('/:id', (req, res) => {
+
+})
+
+// Deleting one
+router.delete('/:id', (req, res) => {
+
+})
+
 module.exports = router
