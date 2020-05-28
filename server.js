a870baff0994d471b45d2368248a5a693abec75c
Sungsoo Kim <sskim950105@gmail.com>
2020-05-28 16:34:57 +0900

    Connect Mongo to Docker

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 require('dotenv').config()
 
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
-
-mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
-
-app.use(express.json())
-
 const eventsRouter = require('./routes/events')
-app.use('/events', eventsRouter)
 
-app.listen(3000, () => {
-    console.log('Server Started')
-})
+mongoose.connect("mongodb://mongo:27017/acemdb", { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        const app = express()
+        const db = mongoose.connection
+        db.on('error', (error) => console.error(error))
+        db.once('open', () => console.log('Connected to Database'))
+        app.use(express.json())
+        app.use('/events', eventsRouter)
+
+        app.listen(3000, () => {
+            console.log('Server Started')
+        })
+    })
+
+
+
+
